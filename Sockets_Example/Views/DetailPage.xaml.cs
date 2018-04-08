b58933504d0c2db73bd4e6749a9731e6ce46e98c
Mark Buttsworrh <mbuttsworth@hotmail.com>
2018-04-08 14:39:31 +0800

    Add project files.

---
+++ Sockets_Example/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Sockets_Example.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Sockets_Example.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ Sockets_Example/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sockets_Example.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Sockets_Example.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
